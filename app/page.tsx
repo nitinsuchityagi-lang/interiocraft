baf9e744623bbab84c291f300008c8a4d911da98
nitinsuchityagi-lang <nitinsuchityagi@gmail.com>
2026-04-25 11:20:04 +0530

    image_change

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Hero from './components/Hero'
+import About from './components/About'
 import Portfolio from './components/Portfolio'
 import Services from './components/Services'
 import ContactForm from './components/ContactForm'
-import About from "./components/About"
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <About />   {/* ✅ THIS WAS MISSING */}
       <Portfolio />
       <Services />
       <ContactForm />
